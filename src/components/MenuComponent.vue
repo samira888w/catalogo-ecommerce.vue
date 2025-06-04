e08fa659abcb97145fc135320be62eac36e9fffd
samira888w <silvaluizsamira87@gmail.com>
2025-06-04 00:34:28 -0300

    começando a pagina de detalhamento

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -2,8 +2,8 @@
   <div class="bg-gray-200 shadow p-6 rounded-lg ">
     <aside class="w-48">
       <h2 class="text-lg font-bold mb-2 align-itens text-center">Categorias</h2>
-      <div>
-        <button @click="$emit('selecionar-categoria', '')" class="bg-white shadow px-16 py-2 mt-6 rounded-lg text-blue-600 hover:underline capitalize">Todos</button>
+      <div class="bg-white shadow px-4 py-2 mt-6 rounded-lg text-blue-600 hover:underline capitalize">
+        <button @click="$emit('selecionar-categoria', '')">Todos</button>
       </div>
       <ul>
         <li v-for="categoria in categorias" :key="categoria">
--- src/components/ProductCardComponent.vue
+++ src/components/ProductCardComponent.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="rounded-lg p-4 text-black shadow-md bg-gray-300 hover:bg-gray-400 hover:shadow-lg">
+  <div class="rounded-lg p-4 text-black shadow-md bg-gray-300 hover:transform hover:scale-105">
     <img :src="produto.thumbnail" :alt="produto.title" class="mx-auto mb-2 h-32 object-cover">
     <h2 class="text-center font-bold">{{ produto.title }}</h2>
     <p class="text-center text-sm">R$ {{ produto.price }}</p>
+    <button @click="$emit('selecionar-produto', produto)" class="btn flex justify-center mt-4 items-center bg-blue-300 text-white py-2 px-6 shadow rounded-lg hover:bg-blue-200">Ver detalhes</button>  
   </div>
+  
 </template>
 
 <script setup>
